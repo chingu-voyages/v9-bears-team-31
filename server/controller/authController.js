090f1c6d7f5bdd380ca44c700ba27f09d5be7b69
karosi12 <adekayor@gmail.com>
2019-06-20 21:35:20 +0100

    try catch block

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -10,28 +10,32 @@ import responses from '../helper/responses';
 
 
 export const login = async (req, res) => {
-  const {
-    phoneNumber,
-    password,
-  } = req.body;
-  const user = await User.findOne({
-    phoneNumber,
-  });
-  if (!user) {
+  try {
+    const {
+      phoneNumber,
+      password,
+    } = req.body;
+    const user = await User.findOne({
+      phoneNumber,
+    });
+    if (!user) {
+      return res.status(400).send(responses.error(400, 'Incorrect phone number or password'));
+    }
+    if (bcrypt.compareSync(password, user.password) && user) {
+      const userObj = {
+        id: user._id,
+        phoneNumber: user.email,
+        firstName: user.firstName,
+      };
+      const token = await generateToken(userObj);
+      const data = {
+        user,
+        token,
+      };
+      return res.status(200).send(responses.success(200, 'User login Successfully', data));
+    }
     return res.status(400).send(responses.error(400, 'Incorrect phone number or password'));
+  } catch (error) {
+    return res.status(400).send(responses.error(400, 'Error occured while logging in'));
   }
-  if (bcrypt.compareSync(password, user.password) && user) {
-    const userObj = {
-      id: user._id,
-      phoneNumber: user.email,
-      firstName: user.firstName,
-    };
-    const token = await generateToken(userObj);
-    const data = {
-      user,
-      token,
-    };
-    return res.status(200).send(responses.success(200, 'User login Successfully', data));
-  }
-  return res.status(400).send(responses.error(400, 'Incorrect phone number or password'));
 };
