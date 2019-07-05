69cd340f3b5ff40057dfdf841b14cb1121bfd48f
emmy1000okello <emmy1000okello@gmail.com>
2019-07-05 12:35:07 +0300

    feature(auth): hotfix login interceptor error

--- client/src/app/services/safariBuddy.interceptor.ts
+++ client/src/app/services/safariBuddy.interceptor.ts
@@ -1,22 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { WebServiceService } from './web-service.service';
 
 
 @Injectable()
 export class SafariBuddyInterceptor implements HttpInterceptor {
-  constructor() {}
+
+  auth: any;
+  constructor(inj: Injector) {
+    this.auth = inj.get(WebServiceService);
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // check if the current user is logged in
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    if (currentUser.success && currentUser.data.token) {
+    const storage = localStorage || sessionStorage;
+    const currentUser = JSON.parse(storage.getItem('currentUser'));
+    console.log('current user', currentUser);
+    if (currentUser) {
+      if (currentUser.success && currentUser.data.token) {
       // clone the incoming request and add JWT token in the cloned request's Authorization Header
       request = request.clone({
           setHeaders: {
               Authorization: `Bearer ${currentUser.data.token}`
           }
       });
+    }
     }
 
     // handle any other requests which went unhandled
