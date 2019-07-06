db646e0f44ef2d52363aef37a9afed4b780913a0
emmy1000okello <emmy1000okello@gmail.com>
2019-07-06 13:13:43 +0300

    feature(hotfix-taxi): add review model

---
+++ server/model/review.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const {
+  Schema,
+} = mongoose;
+const reviewSchema = new Schema({
+  userPhoneNumber: {
+    type: String,
+  },
+  taxiPlateNumber: {
+    type: String,
+  },
+  userReview: {
+    type: Number,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+export default mongoose.model('Review', reviewSchema);
